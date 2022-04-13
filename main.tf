05a7ea2ad253b19a13fd0eec0170419b0d509190
sachin gaikwad <sachinm.gaikwad185@gmail.com>
2022-04-13 15:55:12 +0530

    Added Key-Vault

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ locals {
   inside_vpc  = length(var.vpc_options["subnet_ids"]) > 0 ? true : false
 }
 
+# Create a resource group for security
+resource "azurerm_resource_group" "security-rg" {
+  name     = "security-${var.environment}-rg"
+  location = var.location
+}
+
+
 resource "azurerm_resource_group" "acs_rg" {
   name     = var.acs_rg_name
   location = var.acs_region
@@ -27,3 +34,31 @@ resource "random_id" "acs_unique_id" {
   byte_length = 3
 }
 
+resource "azurerm_key_vault" "acs_key_vault" {
+  name                        = "azureacskeyvault"
+  location                    = azurerm_resource_group.acs_rg.location
+  resource_group_name         = azurerm_resource_group.acs_rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
