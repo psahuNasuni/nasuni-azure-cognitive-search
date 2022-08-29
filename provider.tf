16544be5d1bac315dec1fbaabc97d371eddd18d6
sgaikwadNasuni <101191978+sgaikwadNasuni@users.noreply.github.com>
2022-08-29 17:06:32 +0530

    Removed unwanted provider's features.

--- provider.tf
+++ provider.tf
@@ -9,10 +9,5 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy          = false
-      purge_soft_deleted_secrets_on_destroy = false
-    }
-  }
+  features {}
 }
